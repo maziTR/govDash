275f104b31fee265a7f118eb9ec3699fd5c3fd5e
Sima Margulis Davidi <simamarg@gmail.com>
2018-03-06 06:59:54 +0200

    fix navbar links, sign in with login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExecutionDashboardComponent } from './execution-dashboard/execution-dashboard.component';
 import { BlockersDashboardComponent } from './blockers-dashboard/blockers-dashboard.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'execution', component: ExecutionDashboardComponent },
   { path: 'blockers', component: BlockersDashboardComponent }
 ];
