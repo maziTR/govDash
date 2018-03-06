ea4ece27741de326c31fb415b66a333a75cab5fa
Anael Dahan <anael.assayag@gmail.com>
2018-03-06 17:04:11 +0200

    convert data to correct format for amchart

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { AmChartsService, AmChart } from "@amcharts/amcharts3-angular";
+import { ChartsCalculationsService } from '../charts-calculations.service';
+import { TablesService } from '../tables.service';
 
 @Component({
   selector: 'bar-chart',
@@ -8,10 +10,59 @@ import { AmChartsService, AmChart } from "@amcharts/amcharts3-angular";
 })
 export class BarChartComponent implements OnInit {
   private chart: AmChart;
+  data: any[];
+  sheets:any[];
 
-  constructor(private AmCharts: AmChartsService) { }
+  constructor(private AmCharts: AmChartsService,
+              private test: ChartsCalculationsService,
+              private tableService: TablesService) { }
 
   ngOnInit() {
+    this.test.getSheets();
+    this.tableService.getTables().subscribe(data => {
+      this.sheets = data; 
+      var sheet = this.sheets[0]
+      console.log(this.sheets)
+
+      var field_count = {};
+
+      // var field_array = [];
+      // var chart_array = [];
+      for (var i=1; i<sheet.length; i++){    
+        var field = sheet[i][3];
+        var status = sheet[i][105];
+        if (status) {
+          if(field_count[field]) {
+            if (field_count[field][status]) {
+              field_count[field][status]++
+            }
+            else {
+              field_count[field][status]=1; 
+            }
+
+          }else{
+            field_count[field] = {};
+            field_count[field][status] = 1
+          }
+        }
+        // return field_count
+      }
+      console.log(field_count);
+      var output_array = [];
+
+      for(var name in field_count){ 
+
+      var graph_object ={field: name};
+        var task_details = field_count[name]; 
+        for (status in task_details){
+         graph_object[status] = task_details[status]
+        }
+        output_array.push(graph_object)
+      }
+      console.log(output_array)
+      return output_array
+
+    })
   }
 
   ngAfterViewInit() {
--- src/app/charts-calculations.service.ts
+++ src/app/charts-calculations.service.ts
@@ -1,53 +1,59 @@
 import { Injectable } from '@angular/core';
 import { TablesService } from './tables.service';
-
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ChartsCalculationsService {
   sheets: any[] = [];
-
-
+  // status: string
+  
   constructor(private tableService: TablesService) { }
+  
   getSheets() {
-    this.tableService.getTables().subscribe(data => {
-      this.sheets = data; 
-      console.log(this.sheets)
-      var sheet = this.sheets[0]
-      var field_count = {};
-      var field_array = [];
-      var chart_array = [];
-      for (var i=1; i<sheet.length; i++){    
-        var field = sheet[i][3];
-        var status = sheet[i][105];
-        if (status) {
-          if(field_count[field]) {
-            if (field_count[field][status]) {
-              field_count[field][status]++
-            }
-            else {
-              field_count[field][status]=1; 
-            }
+    // this.tableService.getTables().subscribe(data => {
+    //   this.sheets = data; 
+    //   var sheet = this.sheets[0]
+    //   console.log(this.sheets)
 
-          }else{
-            field_count[field] = {};
-            field_count[field][status] = 1
-          }
-        }
-        // return field_count
-      }
-      field_array.push(field_count)
-      for(var i=0; i<field_array.length; i++) {
-        var eachField = field_array[i] 
-      }
-      console.log(field_array[0])
-      console.log(field_array)
+    //   var field_count = {};
 
+    //   // var field_array = [];
+    //   // var chart_array = [];
+    //   for (var i=1; i<sheet.length; i++){    
+    //     var field = sheet[i][3];
+    //     var status = sheet[i][105];
+    //     if (status) {
+    //       if(field_count[field]) {
+    //         if (field_count[field][status]) {
+    //           field_count[field][status]++
+    //         }
+    //         else {
+    //           field_count[field][status]=1; 
+    //         }
 
+    //       }else{
+    //         field_count[field] = {};
+    //         field_count[field][status] = 1
+    //       }
+    //     }
+    //     // return field_count
+    //   }
+    //   console.log(field_count);
+    //   var output_array = [];
 
+    //   for(var name in field_count){ 
 
+    //   var graph_object ={task_type: name};
+    //     var task_details = field_count[name]; 
+    //     for (status in task_details){
+    //      graph_object[status] = task_details[status]
+    //     }
+    //     output_array.push(graph_object)
+    //   }
+    //   console.log(output_array)
+    //   return output_array
 
-
-
-    })
+    // })
   }
+
 }
