19c7a865bc074981619b2fb37c7c5dd52da4783c
Sima Margulis Davidi <simamarg@gmail.com>
2018-03-21 00:09:32 +0200

    heroku upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   sheetsArray: any[];
   user: {name: string};
 
-  constructor(private chartsService: ChartsService, private userService: UserService) { }
+  constructor(public chartsService: ChartsService, private userService: UserService) { }
 
   ngOnInit() {
     this.chartsService.getTablesRequest().subscribe(data => {
